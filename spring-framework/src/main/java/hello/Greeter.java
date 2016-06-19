1b7524206e79097cad9fa9572981ca9afc45e869
chenxing12 <woshimrf@126.com>
2016-06-19 23:01:31 +0800

    init

---
+++ spring-framework/src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by miaorf on 2016/6/19.
+ */
+public class Greeter {
+    public String syaHello(){
+        return "Hello World!";
+    }
+}
---
+++ spring-framework/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+/**
+ * Created by miaorf on 2016/6/19.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        Greeter greeter = new Greeter();
+        greeter.syaHello();
+    }
+}
